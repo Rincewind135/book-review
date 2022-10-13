a5e01ac2045e19a4a2368e7883609b75393ba12a
tue <GitCommit@Email.com>
2022-10-13 15:58:25 +0200

    Added integrationtests for opretReview/hentReview

--- src/test/java/application/integration/BogIntegrationTest.java
+++ src/test/java/application/integration/BogIntegrationTest.java
@@ -1,27 +1,16 @@
 package application.integration;
 
-import application.dto.*;
-import application.util.DtoTestUtil;
-import application.util.WebTestUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import application.dto.HentBogResponseDto;
+import application.dto.OpretBogResponseDto;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 @AutoConfigureMockMvc
-public class BogIntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
+public class BogIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     public void opretOgHentBog() throws Exception {
@@ -59,55 +48,5 @@ public class BogIntegrationTest {
         titel = UUID.randomUUID().toString();
         opretBogResponse = opretBogMedTitel(titel);
         assertBogBlevOprettet(opretBogResponse);
-    }
-
-    private void assertBogBlevIkkeFundet(HentBogResponseDto hentBogResponse) {
-        assertNotNull(hentBogResponse);
-        assertEquals(ResponseDto.StatusKode.INPUT_FEJL, hentBogResponse.getStatusKode());
-        assertEquals(HentBogResponseDto.StatusSubKode.UKENDT_BOG, hentBogResponse.getStatusSubKode());
-    }
-
-    private void assertBogBlevIkkeOprettet(OpretBogResponseDto opretBogResponse) {
-        assertNull(opretBogResponse);
-    }
-
-    private static void assertBogBlevHentet(HentBogResponseDto hentBogResponse) {
-        assertNotNull(hentBogResponse);
-        assertEquals(ResponseDto.StatusKode.OK, hentBogResponse.getStatusKode());
-    }
-
-    private HentBogResponseDto hentBogMedTitel(String titel) throws Exception {
-        HentBogRequestDto hentRequestDto = DtoTestUtil.newHentBogRequestDto();
-        hentRequestDto.setTitel(titel);
-        return hentBog(hentRequestDto);
-    }
-
-    private static void assertBogBlevOprettet(OpretBogResponseDto opretBogResponse) {
-        assertNotNull(opretBogResponse);
-        assertEquals(ResponseDto.StatusKode.OK, opretBogResponse.getStatusKode());
-    }
-
-    private OpretBogResponseDto opretBogMedTitel(String titel) throws Exception {
-        OpretBogRequestDto opretBogRequestDto = DtoTestUtil.newOpretBogRequestDto();
-        opretBogRequestDto.setTitel(titel);
-        return opretBog(opretBogRequestDto);
-    }
-
-    private OpretBogResponseDto opretBog(OpretBogRequestDto opretBogRequestDto) throws Exception {
-        ResultActions opretBogResult = WebTestUtil.kaldOpretBogEndpoint(mockMvc, opretBogRequestDto);
-        String responseBody = opretBogResult.andReturn().getResponse().getContentAsString();
-        if (StringUtils.isBlank(responseBody)) {
-            return null;
-        }
-        return new ObjectMapper().readValue(responseBody, OpretBogResponseDto.class);
-    }
-
-    private HentBogResponseDto hentBog(HentBogRequestDto hentRequestDto) throws Exception {
-        ResultActions hentBogResult = WebTestUtil.kaldHentBogEndpoint(mockMvc, hentRequestDto);
-        String responseBody = hentBogResult.andReturn().getResponse().getContentAsString();
-        if (StringUtils.isBlank(responseBody)) {
-            return null;
-        }
-        return new ObjectMapper().readValue(responseBody, HentBogResponseDto.class);
     }
 }
